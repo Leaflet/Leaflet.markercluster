81fa74b6db70b2213ce29b03452fc893160eacb2
Tom MacWright <tom@macwright.org>
2013-09-17 18:49:48 -0400

    Add distancegrid spec

---
+++ spec/suites/DistanceGridSpec.js
@@ -0,0 +1,21 @@
+﻿describe('distance grid', function () {
+	it('addObject', function () {
+		var grid = new L.DistanceGrid(100),
+		    obj = {};
+
+		expect(grid.addObject(obj, { x: 0, y: 0 })).to.eql(undefined);
+		expect(grid.removeObject(obj, { x: 0, y: 0 })).to.eql(true);
+	});
+
+	it('eachObject', function (done) {
+		var grid = new L.DistanceGrid(100),
+		    obj = {};
+
+		expect(grid.addObject(obj, { x: 0, y: 0 })).to.eql(undefined);
+
+		grid.eachObject(function(o) {
+			expect(o).to.eql(obj);
+			done();
+		});
+	});
+});
