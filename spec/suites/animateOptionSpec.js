4872c441a376eceab298a16539470bb51faf0c7e
ghybs <ghybs1@gmail.com>
2015-10-15 23:38:46 +0400

    Converted animateOptionSpec test suite
    Following re-work of RefreshSpec test suite, converted this suite as well
    to avoid use of `beforeEach` and `afterEach` hooks which seem to create
    problems with PhantomJS.

--- spec/suites/animateOptionSpec.js
+++ spec/suites/animateOptionSpec.js
@@ -1,28 +1,105 @@
 describe('animate option', function () {
-	var map, div;
 
-	beforeEach(function () {
-		div = document.createElement('div');
-		div.style.width = '200px';
-		div.style.height = '200px';
-		document.body.appendChild(div);
+	/**
+	 * Wrapper for Mocha's `it` function, to avoid using `beforeEach` and `afterEach`
+	 * which create problems with PhantomJS when total number of tests (across all suites)
+	 * increases. Might be due to use of promises for which PhantomJS performs badly?
+	 * @param testDescription string
+	 * @param testInstructions function
+	 * @param testFinally function to be executed just before afterEach2, in the `finally` block.
+	 */
+	function it2(testDescription, testInstructions, testFinally) {
 
-		map = L.map(div, { maxZoom: 18 });
+		it(testDescription, function () {
 
-		// Corresponds to zoom level 8 for the above div dimensions.
-		map.fitBounds(new L.LatLngBounds([
-			[1, 1],
-			[2, 2]
-		]));
-	});
-	afterEach(function () {
-		document.body.removeChild(div);
-	});
+			// Before each test.
+			if (typeof beforeEach2 === "function") {
+				beforeEach2();
+			}
 
-	it('hooks animated methods version by default', function () {
+			try {
+
+				// Perform the actual test instructions.
+				testInstructions();
+
+			} catch (e) {
+
+				// Re-throw the exception so that Mocha sees the failed test.
+				throw e;
+
+			} finally {
+
+				// If specific final instructions are provided.
+				if (typeof testFinally === "function") {
+					testFinally();
+				}
+
+				// After each test.
+				if (typeof afterEach2 === "function") {
+					afterEach2();
+				}
+
+			}
+		});
+	}
+
+
+	/////////////////////////////
+	// SETUP FOR EACH TEST
+	/////////////////////////////
+
+	/**
+	 * Instructions to be executed before each test called with `it2`.
+	 */
+	function beforeEach2() {
+
+		// Nothing for this test suite.
+
+	}
+
+	/**
+	 * Instructions to be executed after each test called with `it2`.
+	 */
+	function afterEach2() {
+
+		if (group instanceof L.MarkerClusterGroup) {
+			group.removeLayers(group.getLayers());
+			map.removeLayer(group);
+		}
+
+		// Throw away group as it can be assigned with different configurations between tests.
+		group = null;
+	}
+
+
+	/////////////////////////////
+	// PREPARATION CODE
+	/////////////////////////////
+
+	var div, map, group;
+
+	div = document.createElement('div');
+	div.style.width = '200px';
+	div.style.height = '200px';
+	document.body.appendChild(div);
+
+	map = L.map(div, { maxZoom: 18 });
+
+	// Corresponds to zoom level 8 for the above div dimensions.
+	map.fitBounds(new L.LatLngBounds([
+		[1, 1],
+		[2, 2]
+	]));
+
+
+	/////////////////////////////
+	// TESTS
+	/////////////////////////////
+
+	it2('hooks animated methods version by default', function () {
 
 		// Need to add to map so that we have the top cluster level created.
-		var group = L.markerClusterGroup().addTo(map);
+		group = L.markerClusterGroup().addTo(map);
 
 		var withAnimation = L.MarkerClusterGroup.prototype._withAnimation;
 
@@ -42,10 +119,10 @@ describe('animate option', function () {
 
 	});
 
-	it('hooks non-animated methods version when set to false', function () {
+	it2('hooks non-animated methods version when set to false', function () {
 
 		// Need to add to map so that we have the top cluster level created.
-		var group = L.markerClusterGroup({animate: false}).addTo(map);
+		group = L.markerClusterGroup({animate: false}).addTo(map);
 
 		var noAnimation = L.MarkerClusterGroup.prototype._noAnimation;
 
@@ -64,5 +141,13 @@ describe('animate option', function () {
 		expect(cluster._animationUnspiderfy).to.be(noAnimation._animationUnspiderfy);
 
 	});
+
+
+	/////////////////////////////
+	// CLEAN UP CODE
+	/////////////////////////////
+
+	map.remove();
+	document.body.removeChild(div);
 
 });
