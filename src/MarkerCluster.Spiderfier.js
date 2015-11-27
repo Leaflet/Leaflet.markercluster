e894b31fc3f0a4e119efda656ca67dd9b1f385d2
ghybs <ghybs1@gmail.com>
2015-11-27 15:34:13 +0400

    Added listener de-binding on remove
    _noanimationUnspiderfy was not de-bound from map "zoomend" event when group is removed from map.

--- src/MarkerCluster.Spiderfier.js
+++ src/MarkerCluster.Spiderfier.js
@@ -361,6 +361,7 @@ L.MarkerClusterGroup.include({
 		this._map.off('click', this._unspiderfyWrapper, this);
 		this._map.off('zoomstart', this._unspiderfyZoomStart, this);
 		this._map.off('zoomanim', this._unspiderfyZoomAnim, this);
+		this._map.off('zoomend', this._noanimationUnspiderfy, this);
 
 		//Ensure that markers are back where they should be
 		// Use no animation to avoid a sticky leaflet-cluster-anim class on mapPane
