645e3a8b3705640f7ada6bf5e10797253e65102d
dave <danzel@localhost.geek.nz>
2017-11-06 10:46:21 +1300

    Bump this off

--- src/copyright.js
+++ src/copyright.js
@@ -1,5 +1,5 @@
 /*
  Leaflet.markercluster, Provides Beautiful Animated Marker Clustering functionality for Leaflet, a JS library for interactive maps.
  https://github.com/Leaflet/Leaflet.markercluster
- (c) 2012-2013, Dave Leaver, smartrak
+ (c) 2012-2017, Dave Leaver
 */
